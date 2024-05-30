09bc6ab8ea03f76dfc0af4ae85474e6853d5ddda
shuvamshr <shuvamshrestha408@gmail.com>
2024-05-30 16:16:57 +1000

    Initial Commit

---
+++ MyPro/MyProApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyProApp.swift
+//  MyPro
+//
+//  Created by HSIN on 30/5/2024.
+//
+
+import SwiftUI
+
+@main
+struct MyProApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
